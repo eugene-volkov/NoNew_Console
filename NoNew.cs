0dd75a87f9ae745728121b26cbd3208d79dac729
Eugene Volkov <bizzar@list.ru>
2021-03-11 00:59:56 +0300

    Add project files.

---
+++ NoNew.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoNew_Console
+{
+    class NoNew
+    {
+        private NoNew()
+        {
+            Console.WriteLine("I'm alive!");
+        }
+
+        public static NoNew CreateInstance()
+        {
+            return new NoNew();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NoNew_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new NoNew(); // error, because constructor of class NoNew is PRIVATE
+            NoNew.CreateInstance();
+        }
+    }
+}
